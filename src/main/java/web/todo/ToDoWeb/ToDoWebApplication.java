8cec9eae48dfa17d63c4bf2a75347d8e1ec1bea2
Matin <matin.parsapour.iam@gmail.com>
2021-12-25 10:37:47 +0330

    Send all basic data

---
+++ src/main/java/web/todo/ToDoWeb/ToDoWebApplication.java
@@ -0,0 +1,13 @@
+package web.todo.ToDoWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/web/todo/ToDoWeb/ToDoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package web.todo.ToDoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
