c1c6b61d9ccadbcfdbcf3f6f191478b802ef0e20
Matin <matin.parsapour.iam@gmail.com>
2021-12-31 19:46:37 +0330

    Add two methods to check if username or email is doplicate

--- src/main/java/web/todo/ToDoWeb/repository/UserRepository.java
+++ src/main/java/web/todo/ToDoWeb/repository/UserRepository.java
@@ -4,4 +4,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import web.todo.ToDoWeb.model.User;
 
 public interface UserRepository extends MongoRepository<User, String> {
+
+    Boolean existsByUserName(String userName);
+
+    Boolean existsByEmail(String email);
 }
