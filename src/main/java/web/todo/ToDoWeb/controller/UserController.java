cdc737e1f29b0bbbd4f84ace983b5eccab31e84c
Matin <matin.parsapour.iam@gmail.com>
2021-12-31 16:14:04 +0330

    Create rest controller for user

---
+++ src/main/java/web/todo/ToDoWeb/controller/UserController.java
@@ -0,0 +1,26 @@
+package web.todo.ToDoWeb.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import web.todo.ToDoWeb.model.User;
+import web.todo.ToDoWeb.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/sign-up")
+    public User signUp(@RequestBody User user){
+        return userService.save(user);
+    }
+}
