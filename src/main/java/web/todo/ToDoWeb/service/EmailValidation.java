93677f9c937af11e1450fecc597c4faba0c19221
Matin <matin.parsapour.iam@gmail.com>
2022-01-01 09:20:54 +0330

    Document the EmailValidation interface

--- src/main/java/web/todo/ToDoWeb/service/EmailValidation.java
+++ src/main/java/web/todo/ToDoWeb/service/EmailValidation.java
@@ -2,5 +2,10 @@ package web.todo.ToDoWeb.service;
 
 public interface EmailValidation {
 
+    /**
+     * Check if email is valid like ******@****.***
+     * @param email check the email provided
+     * @return true if the email is valid
+     */
     Boolean isEmailValid(String email);
 }
