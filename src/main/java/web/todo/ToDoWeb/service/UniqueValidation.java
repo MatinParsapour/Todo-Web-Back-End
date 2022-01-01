bda7bd46a9adb544c5063e4eb7081f174b2b499d
Matin <matin.parsapour.iam@gmail.com>
2022-01-01 09:22:36 +0330

    Document UniqueValidation interface

--- src/main/java/web/todo/ToDoWeb/service/UniqueValidation.java
+++ src/main/java/web/todo/ToDoWeb/service/UniqueValidation.java
@@ -2,8 +2,18 @@ package web.todo.ToDoWeb.service;
 
 public interface UniqueValidation {
 
+    /**
+     * Check if username is doplicate or not
+     * @param userName the username user provided
+     * @return true if the username is doplicate
+     */
     Boolean existsByUserName(String userName);
 
+    /**
+     * Check if email is doplicate or not
+     * @param email the email user provided
+     * @return true if the email is doplicate
+     */
     Boolean existsByEmail(String email);
 
 
