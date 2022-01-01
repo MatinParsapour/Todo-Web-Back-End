67a1284fa30a1db01af3bab306b505cffc95adc3
Matin <matin.parsapour.iam@gmail.com>
2022-01-01 09:26:31 +0330

    Document the save method

--- src/main/java/web/todo/ToDoWeb/service/UserService.java
+++ src/main/java/web/todo/ToDoWeb/service/UserService.java
@@ -1,9 +1,19 @@
 package web.todo.ToDoWeb.service;
 
 import web.todo.ToDoWeb.model.User;
+import web.todo.ToDoWeb.model.dto.UserDTO;
 import web.todo.ToDoWeb.model.dto.UserSignUpDTO;
 
 public interface UserService extends BaseService<User, String> {
 
+    /**
+     * Save data in database
+     * Throws a doplicate exception if username or email is doplicate
+     * Throws an empty exception if one or more of mandatory fields are empty
+     * Throws an invalid exception if the email provided is invalid
+     * @param userSignUpDTO the user with first name and last name and username and email and password
+     * @return the user saved in database
+     * @throws Exception the exception for encrypting password
+     */
     User saveDTO(UserSignUpDTO userSignUpDTO) throws Exception;
 }
