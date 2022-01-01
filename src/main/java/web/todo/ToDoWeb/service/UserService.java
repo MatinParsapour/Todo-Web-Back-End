b4c1a258b2d0a33646b4740ddfdffb53afb5685f
Matin <matin.parsapour.iam@gmail.com>
2022-01-01 09:26:47 +0330

    Add a new method to update user

--- src/main/java/web/todo/ToDoWeb/service/UserService.java
+++ src/main/java/web/todo/ToDoWeb/service/UserService.java
@@ -16,4 +16,16 @@ public interface UserService extends BaseService<User, String> {
      * @throws Exception the exception for encrypting password
      */
     User saveDTO(UserSignUpDTO userSignUpDTO) throws Exception;
+
+    /**
+     * Update the user saved in data base
+     * Throws a not found exception if no id provided
+     * Throws an empty exception if one or more of mandatory fields are empty
+     * Throws a doplicate exception if username or email if doplicate
+     * Throws an invalid exception if email is invalid
+     * @param userDTO the user with all of the field like phone number last name....
+     * @return the user saved in database
+     * @throws Exception the exception for encrypting password
+     */
+    User updateDTO(UserDTO userDTO) throws Exception;
 }
