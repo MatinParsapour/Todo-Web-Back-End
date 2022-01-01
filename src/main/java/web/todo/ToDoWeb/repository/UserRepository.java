6e5b864708eef00c6118e0a2642682ef5d7a9763
Matin <matin.parsapour.iam@gmail.com>
2022-01-01 09:57:07 +0330

    Find the user that has username and password and is not deleted

--- src/main/java/web/todo/ToDoWeb/repository/UserRepository.java
+++ src/main/java/web/todo/ToDoWeb/repository/UserRepository.java
@@ -2,10 +2,13 @@ package web.todo.ToDoWeb.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import web.todo.ToDoWeb.model.User;
+import web.todo.ToDoWeb.model.dto.UserDTO;
 
 public interface UserRepository extends MongoRepository<User, String> {
 
     Boolean existsByUserName(String userName);
 
     Boolean existsByEmail(String email);
+
+    UserDTO findByUserNameAndPasswordAndIsDeletedFalse(String username, String password);
 }
