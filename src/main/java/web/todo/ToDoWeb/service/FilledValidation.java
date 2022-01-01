a98f86d7c553f57d18f749ca010533cf0521628f
Matin <matin.parsapour.iam@gmail.com>
2022-01-01 09:21:16 +0330

    Document the FilledValidation interface

--- src/main/java/web/todo/ToDoWeb/service/FilledValidation.java
+++ src/main/java/web/todo/ToDoWeb/service/FilledValidation.java
@@ -2,12 +2,32 @@ package web.todo.ToDoWeb.service;
 
 public interface FilledValidation {
 
+    /**
+     * Check if one field is empty or not
+     * @param field any attribute needs to check
+     * @return true if the field is empty
+     */
     Boolean isEmpty(String field);
 
+    /**
+     * Check if one field is blank or not
+     * @param field any attribute needs to check
+     * @return true if the field is blank
+     */
     Boolean isBlank(String field);
 
+    /**
+     * Check if one field is null or not
+     * @param field any attribute needs to check
+     * @return true if the field is null
+     */
     Boolean isNull(String field);
 
+    /**
+     * Check if the erase white spaces the field is empty or not
+     * @param field any attributes needs to check
+     * @return true if the filed has only white spaces
+     */
     Boolean isWhiteSpace(String field);
 
 }
