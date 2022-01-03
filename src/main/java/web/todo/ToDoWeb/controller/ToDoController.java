85a717985f331a949e7bc66d7b4e68120188781e
Matin <matin.parsapour.iam@gmail.com>
2022-01-03 08:25:05 +0330

    Create a controller for to do to handle requests and add a method to get data from client and save to do to database

---
+++ src/main/java/web/todo/ToDoWeb/controller/ToDoController.java
@@ -0,0 +1,31 @@
+package web.todo.ToDoWeb.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import web.todo.ToDoWeb.model.ToDo;
+import web.todo.ToDoWeb.model.dto.UserDTO;
+import web.todo.ToDoWeb.service.ToDoService;
+
+@RestController
+@RequestMapping("/to-do")
+public class ToDoController {
+
+    private final ToDoService toDoService;
+
+    @Autowired
+    public ToDoController(ToDoService toDoService) {
+        this.toDoService = toDoService;
+    }
+
+    /**
+     * Add to do to the list
+     * @param toDo That at least must include task
+     * @param listName name of list to do add to it
+     * @param folderName name of folder list belongs to
+     * @param username username of user folder belongs to
+     */
+    @PostMapping("/add-to-do/{listName}/folder/{folderName}/for/{username}")
+    public void addToDo(@RequestBody ToDo toDo, @PathVariable("listName") String listName, @PathVariable("folderName") String folderName, @PathVariable("username") String username){
+        toDoService.addToDo(toDo, listName, folderName, username);
+    }
+}
