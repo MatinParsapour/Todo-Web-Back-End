786ba4da3df6720b52d335da11f550b5a69da076
Matin <matin.parsapour.iam@gmail.com>
2022-01-04 15:09:40 +0330

    Update categories to implement the interface

--- src/main/java/web/todo/ToDoWeb/model/Tasks.java
+++ src/main/java/web/todo/ToDoWeb/model/Tasks.java
@@ -4,21 +4,27 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 @Document(collection = "Tasks")
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Tasks{
+public class Tasks implements Category{
+
+    @Id
+    private String id;
 
     @DBRef
     private User user;
 
     @DBRef
-    private Set<ToDo> toDos;
+    private Set<ToDo> toDos = new HashSet<>();
 }
