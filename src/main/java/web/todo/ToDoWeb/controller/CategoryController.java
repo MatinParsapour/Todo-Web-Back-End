beb77cebe149079eac34fa7ae9e9e0540f13e1c0
Matin <matin.parsapour.iam@gmail.com>
2022-01-04 16:21:43 +0330

    Create a method for CategoryController to get data and return

--- src/main/java/web/todo/ToDoWeb/controller/CategoryController.java
+++ src/main/java/web/todo/ToDoWeb/controller/CategoryController.java
@@ -1,18 +1,39 @@
 package web.todo.ToDoWeb.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import web.todo.ToDoWeb.service.impl.CategoryFactoryImpl;
+import org.springframework.web.bind.annotation.*;
+import web.todo.ToDoWeb.model.Category;
+import web.todo.ToDoWeb.model.User;
+import web.todo.ToDoWeb.model.dto.UserDTO;
+import web.todo.ToDoWeb.service.CategoryFactory;
+import web.todo.ToDoWeb.service.UserService;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
 
-    private final CategoryFactoryImpl categoryFactoryImpl;
+    private final CategoryFactory categoryFactory;
+    private final UserService userService;
 
     @Autowired
-    public CategoryController(CategoryFactoryImpl categoryFactoryImpl) {
-        this.categoryFactoryImpl = categoryFactoryImpl;
+    public CategoryController(CategoryFactory categoryFactory, UserService userService) {
+        this.categoryFactory = categoryFactory;
+        this.userService = userService;
+    }
+
+    /**
+     * Get category name and user dto and find todos relate to
+     * category name and user
+     * @param categoryName name of category user looking for
+     * @param userDTO include id
+     * @return a list of categories
+     */
+    @GetMapping("/get-category-to-dos/{categoryName}")
+    public List<Category> getCategoryToDos(@PathVariable("categoryName") String categoryName,@RequestBody UserDTO userDTO){
+        Optional<User> user = userService.findById(userDTO.getId());
+        return categoryFactory.getToDosByCategory(categoryName, user.get());
     }
 }
