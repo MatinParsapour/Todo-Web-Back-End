d2874f1441e1544ae4752175dcd4b0ac767763b7
Matin <matin.parsapour.iam@gmail.com>
2022-01-04 16:24:02 +0330

    Update return type of category

--- src/main/java/web/todo/ToDoWeb/service/CategoryService.java
+++ src/main/java/web/todo/ToDoWeb/service/CategoryService.java
@@ -4,9 +4,11 @@ import web.todo.ToDoWeb.model.Category;
 import web.todo.ToDoWeb.model.ToDo;
 import web.todo.ToDoWeb.model.User;
 
+import java.util.List;
+
 public interface CategoryService<T extends Category>{
 
     void add(ToDo toDo, User user);
 
-    T get(User user);
+    List<T> get(User user);
 }
