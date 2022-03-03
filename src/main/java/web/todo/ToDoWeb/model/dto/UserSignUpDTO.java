6d41c85b3deca26de7b929b25fa52ce285465e3e
Matin <matin.parsapour.iam@gmail.com>
2022-03-03 12:44:26 +0330

    Update user sign up dto to get last name instead of birthday

--- src/main/java/web/todo/ToDoWeb/model/dto/UserSignUpDTO.java
+++ src/main/java/web/todo/ToDoWeb/model/dto/UserSignUpDTO.java
@@ -13,11 +13,11 @@ public class UserSignUpDTO {
 
     private String firstName;
 
+    private String lastName;
+
     private String userName;
 
     private String email;
 
     private String password;
-
-    private String birthDay;
 }
