757dfd24bf6996f289ee3681877e27b7516bfaeb
Matin <matin.parsapour.iam@gmail.com>
2022-03-03 17:29:14 +0330

    Update verify email method

--- src/main/java/web/todo/ToDoWeb/service/impl/EmailConfirmationServiceImpl.java
+++ src/main/java/web/todo/ToDoWeb/service/impl/EmailConfirmationServiceImpl.java
@@ -1,37 +1,48 @@
 package web.todo.ToDoWeb.service.impl;
 
 import lombok.AllArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import web.todo.ToDoWeb.model.dto.UserSignUpDTO;
 import web.todo.ToDoWeb.service.EmailConfirmationService;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
 import java.util.Random;
 
 @Service
 @AllArgsConstructor
 public class EmailConfirmationServiceImpl implements EmailConfirmationService {
 
-    private static Integer code;
+    private static String email;
     private final JavaMailSender sender;
 
-    public static int getCode() {
-        return code;
-    }
 
-    static void setCodeZero(){code = null;}
+    public void sendEmailToVerifyUser(String email) throws MessagingException, UnsupportedEncodingException {
+        String toAddress = email;
+        String fromAddress = "matin.parsapour.iam@gmail.com";
+        String senderName = "My company";
+        String subject = "Please verify your registration";
+        String content = "Hello dear,<br>"
+                + "Please click the link below to verify your registration:<br>"
+                + "<h3><a href=\"[[URL]]\" target=\"_self\">VERIFY</a></h3>"
+                + "Thank you,<br>"
+                + "Your company name.";
 
+        MimeMessage message = sender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
 
-    @Override
-    public void sendEmail(String to, String messageText) {
-        createCode();
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("matin.parsapour.iam@gmail.com");
-        message.setTo(to);
-        String text = messageText + code;
-        message.setText(text);
-        message.setSubject("Confirm your email");
+        helper.setFrom(fromAddress, senderName);
+        helper.setTo(toAddress);
+        helper.setSubject(subject);
+
+        String verifyURL = "http://localhost:4200/validate-email?email=" + email ;
+
+        content = content.replace("[[URL]]", verifyURL);
+
+        helper.setText(content, true);
+
         sender.send(message);
         Thread thread = new Thread(new Timer());
         thread.start();
