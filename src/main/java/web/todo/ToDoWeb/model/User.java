627e4ee44755767e8df2b70f70121d0728c00ae9
Matin <matin.parsapour.iam@gmail.com>
2022-03-05 08:20:02 +0330

    update user to return null if there's no birthday

--- src/main/java/web/todo/ToDoWeb/model/User.java
+++ src/main/java/web/todo/ToDoWeb/model/User.java
@@ -53,9 +53,12 @@ public class User {
         return AES.decrypt(password);
     }
 
-    public int getAge() {
-        LocalDate birthday = LocalDate.parse(birthDay);
-        LocalDate now = LocalDate.now();
-        return birthday.until(now).getYears();
+    public Integer getAge() {
+        if (birthDay != null) {
+            LocalDate birthday = LocalDate.parse(birthDay);
+            LocalDate now = LocalDate.now();
+            return birthday.until(now).getYears();
+        }
+        return null;
     }
 }
