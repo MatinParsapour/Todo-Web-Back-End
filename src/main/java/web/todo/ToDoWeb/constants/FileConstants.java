7be433ba2c069e75b30426f4321d9f73b49f6df8
Matin <matin.parsapour.iam@gmail.com>
2022-03-08 21:46:15 +0330

    Update File constant class for to do

--- src/main/java/web/todo/ToDoWeb/constants/FileConstants.java
+++ src/main/java/web/todo/ToDoWeb/constants/FileConstants.java
@@ -2,9 +2,11 @@ package web.todo.ToDoWeb.constants;
 
 public class FileConstants {
     public static final String USER_FOLDER = System.getProperty("D:\\pictures") + "/users/user/";
+    public static final String TODO_FOLDER = System.getProperty("D:\\pictures") + "/todos/todo/";
     public static final String DIRECTORY_CREATED = "Create directory for: ";
     public static final String DOT = ".";
     public static final String JPG_EXTENSION = "jpg";
     public static final String USER_IMAGE_PATH = "/user/image/";
+    public static final String TODO_IMAGE_PATH = "/to-do/image/";
     public static final String FORWARD_SLASH = "/";
 }
