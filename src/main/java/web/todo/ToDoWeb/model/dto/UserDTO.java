9a2bf163ba5abec20e78819185ed9a1ad5b5e296
Matin <matin.parsapour.iam@gmail.com>
2022-03-11 14:42:28 +0330

    Add validation constraints to user dto

--- src/main/java/web/todo/ToDoWeb/model/dto/UserDTO.java
+++ src/main/java/web/todo/ToDoWeb/model/dto/UserDTO.java
@@ -4,8 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import web.todo.ToDoWeb.util.AES;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Getter
@@ -16,12 +19,25 @@ public class UserDTO {
 
     private String id;
 
+    @NotNull(message = "You should enter first name")
+    @NotBlank(message = "You should enter first name")
+    @NotEmpty(message = "You should enter first name")
     private String firstName;
 
+    @NotNull(message = "You should enter last name")
+    @NotBlank(message = "You should enter last name")
+    @NotEmpty(message = "You should enter last name")
     private String lastName;
 
+    @NotNull(message = "You should enter username")
+    @NotBlank(message = "You should enter username")
+    @NotEmpty(message = "You should enter username")
     private String userName;
 
+    @NotNull(message = "You should enter email")
+    @NotBlank(message = "You should enter email")
+    @NotEmpty(message = "You should enter email")
+    @Email(message = "This is not valid structure for an email")
     private String email;
 
     private Long phoneNumber;
@@ -33,7 +49,7 @@ public class UserDTO {
     private transient int age;
 
     public Integer getAge() {
-        if (birthDay != null){
+        if (birthDay != null) {
             LocalDate birthday = LocalDate.parse(birthDay);
             LocalDate now = LocalDate.now();
             return birthday.until(now).getYears();
