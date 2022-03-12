39ed5a26315ab74a16e520e596faab29fd41bbb1
Matin <matin.parsapour.iam@gmail.com>
2022-03-12 14:57:53 +0330

    Add a class for log in

---
+++ src/main/java/web/todo/ToDoWeb/model/dto/UserLoginDTO.java
@@ -0,0 +1,28 @@
+package web.todo.ToDoWeb.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginDTO {
+
+    @NotNull(message = "You should enter username")
+    @NotBlank(message = "You should enter username")
+    @NotEmpty(message = "You should enter username")
+    private String userName;
+
+    @NotNull(message = "You should enter password")
+    @NotBlank(message = "You should enter password")
+    @NotEmpty(message = "You should enter password")
+    @Pattern(regexp = "^(?=(.*[a-z])+)(?=(.*[A-Z])+)(?=(.*[0-9])+)(?=(.*[!@#$%^&*()\\-__+.])+).{8,}$", message = "Please choose a stronger password")
+    private String password;
+}
