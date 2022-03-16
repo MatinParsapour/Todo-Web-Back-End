a753a22d7fef9ceede25216b1a8a42c38246d007
Matin <matin.parsapour.iam@gmail.com>
2022-03-16 17:25:31 +0330

    Implement code generator interface

---
+++ src/main/java/web/todo/ToDoWeb/service/impl/CodeGeneratorServiceImpl.java
@@ -0,0 +1,31 @@
+package web.todo.ToDoWeb.service.impl;
+
+import org.springframework.stereotype.Service;
+import web.todo.ToDoWeb.service.CodeGeneratorService;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@Service
+public class CodeGeneratorServiceImpl implements CodeGeneratorService {
+
+    private static final String ALGORITHM = "AaBbCcDdEeFfGgHhIiJjKkL%^*lMmNnOoPpQqRrSsTtUuVvWwXxYyZz!@#$";
+
+
+    @Override
+    public String generateString() {
+        StringBuilder token = new StringBuilder();
+        int i = 0;
+        do {
+            int position = ThreadLocalRandom.current().nextInt(0, ALGORITHM.length() -1 );
+            char character = ALGORITHM.charAt(position);
+            token.append(character);
+            i++;
+        } while (i <= 20);
+        return token.toString();
+    }
+
+    @Override
+    public Integer generateNumber() {
+        return ThreadLocalRandom.current().nextInt(10000,99999);
+    }
+}
