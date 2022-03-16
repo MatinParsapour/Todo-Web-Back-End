74ab40d46111574756e58fc62da5948a7fc2d450
Matin <matin.parsapour.iam@gmail.com>
2022-03-16 18:10:36 +0330

    Use cache code service to validate code is ok or not

--- src/main/java/web/todo/ToDoWeb/service/impl/EmailServiceImpl.java
+++ src/main/java/web/todo/ToDoWeb/service/impl/EmailServiceImpl.java
@@ -2,9 +2,11 @@ package web.todo.ToDoWeb.service.impl;
 
 import org.springframework.stereotype.Service;
 import web.todo.ToDoWeb.exception.EmptyException;
+import web.todo.ToDoWeb.exception.InValidException;
 import web.todo.ToDoWeb.exception.NotFoundException;
 import web.todo.ToDoWeb.model.User;
 import web.todo.ToDoWeb.repository.UserRepository;
+import web.todo.ToDoWeb.service.CacheCodeService;
 import web.todo.ToDoWeb.service.EmailConfirmationService;
 import web.todo.ToDoWeb.service.EmailService;
 
@@ -15,12 +17,14 @@ import java.io.UnsupportedEncodingException;
 public class EmailServiceImpl extends BaseServiceImpl<User, String, UserRepository> implements EmailService {
 
     private final UserRepository userRepository;
+    private final CacheCodeService cacheCodeService;
     private final EmailConfirmationService emailConfirmationService;
     private User user;
 
-    public EmailServiceImpl(UserRepository repository, UserRepository userRepository, EmailConfirmationService emailConfirmationService) {
+    public EmailServiceImpl(UserRepository repository, UserRepository userRepository, CacheCodeService cacheCodeService, EmailConfirmationService emailConfirmationService) {
         super(repository);
         this.userRepository = userRepository;
+        this.cacheCodeService = cacheCodeService;
         this.emailConfirmationService = emailConfirmationService;
     }
 
@@ -42,7 +46,11 @@ public class EmailServiceImpl extends BaseServiceImpl<User, String, UserReposito
     }
 
     @Override
-    public Boolean validateAndChangeEmail(String email) {
+    public Boolean validateAndChangeEmail(String email, String code) {
+        String emailCode = cacheCodeService.getEmailCode(email);
+        if (!emailCode.equals(code)){
+            throw new InValidException("The code is invalid");
+        }
         if (email.equals(user.getEmail())) {
             userRepository.save(user);
             return true;
