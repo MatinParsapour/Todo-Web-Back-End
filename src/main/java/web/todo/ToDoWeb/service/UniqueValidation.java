3d7cf51dc5bab4082eac575d9252160c92e08647
Matin <matin.parsapour.iam@gmail.com>
2022-03-17 11:18:57 +0330

    Delete username validation in UniqueValidation interface

--- src/main/java/web/todo/ToDoWeb/service/UniqueValidation.java
+++ src/main/java/web/todo/ToDoWeb/service/UniqueValidation.java
@@ -3,13 +3,6 @@ package web.todo.ToDoWeb.service;
 public interface UniqueValidation {
 
     /**
-     * Check if username is doplicate or not
-     * @param userName the username user provided
-     * @return true if the username is doplicate
-     */
-    Boolean existsByUserName(String userName);
-
-    /**
      * Check if email is doplicate or not
      * @param email the email user provided
      * @return true if the email is doplicate
