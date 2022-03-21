bdad1bd3ab35355dd74d118c26df206b88173303
Matin <matin.parsapour.iam@gmail.com>
2022-03-21 16:35:59 +0330

    Add two methods in email repository to find for inbox and outbox

--- src/main/java/web/todo/ToDoWeb/repository/EmailRepository.java
+++ src/main/java/web/todo/ToDoWeb/repository/EmailRepository.java
@@ -3,5 +3,11 @@ package web.todo.ToDoWeb.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import web.todo.ToDoWeb.model.Email;
 
+import java.util.List;
+
 public interface EmailRepository extends MongoRepository<Email, String> {
+
+    List<Email> findAllByOriginAndIsDeletedFalse(String origin);
+
+    List<Email> findAllByDestinationAndIsDeletedFalse(String origin);
 }
