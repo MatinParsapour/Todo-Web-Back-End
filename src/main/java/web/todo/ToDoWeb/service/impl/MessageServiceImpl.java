ccc143ad1f8fda6aaf55725f1ebbd0e029deacd8
Matin <matin.parsapour.iam@gmail.com>
2022-03-27 12:57:03 +0430

    Mark message service impl with service annotation

--- src/main/java/web/todo/ToDoWeb/service/impl/MessageServiceImpl.java
+++ src/main/java/web/todo/ToDoWeb/service/impl/MessageServiceImpl.java
@@ -1,9 +1,11 @@
 package web.todo.ToDoWeb.service.impl;
 
+import org.springframework.stereotype.Service;
 import web.todo.ToDoWeb.model.Message;
 import web.todo.ToDoWeb.repository.MessageRepository;
 import web.todo.ToDoWeb.service.MessageService;
 
+@Service
 public class MessageServiceImpl extends BaseServiceImpl<Message, String, MessageRepository> implements MessageService {
 
     public MessageServiceImpl(MessageRepository repository) {
