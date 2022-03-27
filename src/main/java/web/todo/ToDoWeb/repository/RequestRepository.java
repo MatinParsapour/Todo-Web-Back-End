eb826551b01db759580e41379a5ae962b58f37ff
Matin <matin.parsapour.iam@gmail.com>
2022-03-27 16:54:35 +0430

    Add a method in RequestRepository to find request by id and isn't deleted

--- src/main/java/web/todo/ToDoWeb/repository/RequestRepository.java
+++ src/main/java/web/todo/ToDoWeb/repository/RequestRepository.java
@@ -6,8 +6,11 @@ import web.todo.ToDoWeb.model.Request;
 import web.todo.ToDoWeb.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RequestRepository extends MongoRepository<Request, String> {
 
     List<Request> findAllByUser(User user);
+
+    Optional<Request> findByIdAndIsDeletedFalse(String requestId);
 }
