bc6916036db0289fb18ef63246cc362a63096ebb
Matin <matin.parsapour.iam@gmail.com>
2022-03-27 13:20:10 +0430

    Add a method to save new request in data base

--- src/main/java/web/todo/ToDoWeb/service/RequestService.java
+++ src/main/java/web/todo/ToDoWeb/service/RequestService.java
@@ -1,6 +1,9 @@
 package web.todo.ToDoWeb.service;
 
 import web.todo.ToDoWeb.model.Request;
+import web.todo.ToDoWeb.model.dto.RequestDTO;
 
 public interface RequestService extends BaseService<Request, String> {
+
+    void startNewRequest(RequestDTO requestDTO);
 }
