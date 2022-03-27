9d7afac1b4223ceb37e7d9806aaad6be17a91a36
Matin <matin.parsapour.iam@gmail.com>
2022-03-27 12:45:15 +0430

    Update request dto

--- src/main/java/web/todo/ToDoWeb/model/dto/RequestDTO.java
+++ src/main/java/web/todo/ToDoWeb/model/dto/RequestDTO.java
@@ -1,5 +1,22 @@
 package web.todo.ToDoWeb.model.dto;
 
+import lombok.Data;
+import web.todo.ToDoWeb.enumeration.Priority;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Data
 public class RequestDTO {
 
+    @NotEmpty(message = "You should fill topic")
+    @NotBlank(message = "You should fill topic")
+    @NotNull(message = "You should fill topic")
+    private String topic;
+
+    @NotBlank(message = "You should fill priority")
+    @NotEmpty(message = "You should fill priority")
+    @NotNull(message = "You should fill priority")
+    private Priority priority;
 }
