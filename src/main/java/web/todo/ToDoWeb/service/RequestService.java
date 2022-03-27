f71a0d070c34b467cad0a1bbc204b447a437be7f
Matin <matin.parsapour.iam@gmail.com>
2022-03-27 16:34:20 +0430

    Add a method in RequestService to return list of user requests

--- src/main/java/web/todo/ToDoWeb/service/RequestService.java
+++ src/main/java/web/todo/ToDoWeb/service/RequestService.java
@@ -3,7 +3,11 @@ package web.todo.ToDoWeb.service;
 import web.todo.ToDoWeb.model.Request;
 import web.todo.ToDoWeb.model.dto.RequestDTO;
 
+import java.util.List;
+
 public interface RequestService extends BaseService<Request, String> {
 
     void startNewRequest(RequestDTO requestDTO);
+
+    List<Request> getUserRequests(String userId);
 }
