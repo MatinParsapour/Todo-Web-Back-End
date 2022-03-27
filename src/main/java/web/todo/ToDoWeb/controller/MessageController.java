2140f088c30a0aa80a6f9a69e0a6e02902578c94
Matin <matin.parsapour.iam@gmail.com>
2022-03-27 17:51:27 +0430

    Add a method in controller to update message

--- src/main/java/web/todo/ToDoWeb/controller/MessageController.java
+++ src/main/java/web/todo/ToDoWeb/controller/MessageController.java
@@ -1,6 +1,7 @@
 package web.todo.ToDoWeb.controller;
 
 import org.springframework.web.bind.annotation.*;
+import web.todo.ToDoWeb.model.Message;
 import web.todo.ToDoWeb.model.dto.MessageDTO;
 import web.todo.ToDoWeb.service.MessageService;
 
@@ -17,5 +18,10 @@ public class MessageController {
     @PostMapping("/send-message/{requestId}")
     public void sendMessage(@PathVariable("requestId") String requestId, @RequestBody MessageDTO messageDTO){
         messageService.sendMessage(requestId, messageDTO);
+    }
+
+    @PutMapping("/update-message")
+    public void updateMessage(@RequestBody Message message){
+        messageService.updateMessage(message);
     }
 }
