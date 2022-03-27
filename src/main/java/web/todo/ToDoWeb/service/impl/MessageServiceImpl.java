61dcd1aa0dd7e77ededa845bf70052136095908e
Matin <matin.parsapour.iam@gmail.com>
2022-03-27 17:38:11 +0430

    Implement send message method to save message in database and send to request service to save in request

--- src/main/java/web/todo/ToDoWeb/service/impl/MessageServiceImpl.java
+++ src/main/java/web/todo/ToDoWeb/service/impl/MessageServiceImpl.java
@@ -1,14 +1,33 @@
 package web.todo.ToDoWeb.service.impl;
 
 import org.springframework.stereotype.Service;
+import web.todo.ToDoWeb.exception.NotFoundException;
 import web.todo.ToDoWeb.model.Message;
+import web.todo.ToDoWeb.model.User;
+import web.todo.ToDoWeb.model.dto.MessageDTO;
 import web.todo.ToDoWeb.repository.MessageRepository;
 import web.todo.ToDoWeb.service.MessageService;
+import web.todo.ToDoWeb.service.RequestService;
+import web.todo.ToDoWeb.service.UserService;
 
 @Service
 public class MessageServiceImpl extends BaseServiceImpl<Message, String, MessageRepository> implements MessageService {
 
-    public MessageServiceImpl(MessageRepository repository) {
+    private final MessageRepository messageRepository;
+    private final RequestService requestService;
+    private final UserService userService;
+
+    public MessageServiceImpl(MessageRepository repository, MessageRepository messageRepository, RequestService requestService, UserService userService) {
         super(repository);
+        this.messageRepository = messageRepository;
+        this.requestService = requestService;
+        this.userService = userService;
+    }
+
+    @Override
+    public void sendMessage(String requestId, MessageDTO messageDTO) {
+        Message message = initializeMessageByMessageDTO(messageDTO);
+        Message newMessage = save(message);
+        requestService.addMessageToRequest(requestId, newMessage);
     }
 }
