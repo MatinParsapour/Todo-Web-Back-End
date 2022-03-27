74ef1833099dd850d3c803347b7721658cc7c0be
Matin <matin.parsapour.iam@gmail.com>
2022-03-27 16:33:56 +0430

    Add a method in repository to find all of user requests by user

--- src/main/java/web/todo/ToDoWeb/repository/RequestRepository.java
+++ src/main/java/web/todo/ToDoWeb/repository/RequestRepository.java
@@ -1,7 +1,13 @@
 package web.todo.ToDoWeb.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import web.todo.ToDoWeb.model.Request;
+import web.todo.ToDoWeb.model.User;
+
+import java.util.List;
 
 public interface RequestRepository extends MongoRepository<Request, String> {
+
+    List<Request> findAllByUser(User user);
 }
