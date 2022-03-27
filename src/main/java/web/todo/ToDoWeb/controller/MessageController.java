a667d17f67c4826194371b58450162a3c98b2fbc
Matin <matin.parsapour.iam@gmail.com>
2022-03-27 17:36:55 +0430

    Add a method in controller for user to send a new message

--- src/main/java/web/todo/ToDoWeb/controller/MessageController.java
+++ src/main/java/web/todo/ToDoWeb/controller/MessageController.java
@@ -1,9 +1,21 @@
 package web.todo.ToDoWeb.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import web.todo.ToDoWeb.model.dto.MessageDTO;
+import web.todo.ToDoWeb.service.MessageService;
 
 @RestController
 @RequestMapping("/message")
 public class MessageController {
+
+    private final MessageService messageService;
+
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @PostMapping("/send-message/{requestId}")
+    public void sendMessage(@PathVariable("requestId") String requestId, @RequestBody MessageDTO messageDTO){
+        messageService.sendMessage(requestId, messageDTO);
+    }
 }
