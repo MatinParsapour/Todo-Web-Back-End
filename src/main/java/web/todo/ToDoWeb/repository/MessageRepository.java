daf08a5cce9e9c9c88023c1e3ee447de388ebadf
Matin <matin.parsapour.iam@gmail.com>
2022-03-27 17:52:16 +0430

    Add a method to check if a message exists by id and isn't deleted

--- src/main/java/web/todo/ToDoWeb/repository/MessageRepository.java
+++ src/main/java/web/todo/ToDoWeb/repository/MessageRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import web.todo.ToDoWeb.model.Message;
 
 public interface MessageRepository extends MongoRepository<Message, String> {
+
+    Boolean existsByIdAndIsDeletedFalse(String messageId);
 }
