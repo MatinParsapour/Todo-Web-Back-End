8bad596fc2c2f101dc7d503108dc1117ab87ea0e
Matin <matin.parsapour.iam@gmail.com>
2022-03-27 13:21:34 +0430

    Add new method to get new request and send to service to save in data base

--- src/main/java/web/todo/ToDoWeb/controller/RequestController.java
+++ src/main/java/web/todo/ToDoWeb/controller/RequestController.java
@@ -1,4 +1,28 @@
 package web.todo.ToDoWeb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import web.todo.ToDoWeb.model.dto.RequestDTO;
+import web.todo.ToDoWeb.service.RequestService;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/request")
 public class RequestController {
+
+    private final RequestService requestService;
+
+    @Autowired
+    public RequestController(RequestService requestService) {
+        this.requestService = requestService;
+    }
+
+    @PostMapping("/start-new-request")
+    public void startNewRequest(@Valid @RequestBody RequestDTO requestDTO){
+        requestService.startNewRequest(requestDTO);
+    }
 }
