b0d046fd309364653e61d16833291f3296b2ec95
Matin <matin.parsapour.iam@gmail.com>
2022-03-27 12:57:11 +0430

    Mark request service impl with service annotation

--- src/main/java/web/todo/ToDoWeb/service/impl/RequestServiceImpl.java
+++ src/main/java/web/todo/ToDoWeb/service/impl/RequestServiceImpl.java
@@ -1,9 +1,11 @@
 package web.todo.ToDoWeb.service.impl;
 
+import org.springframework.stereotype.Service;
 import web.todo.ToDoWeb.model.Request;
 import web.todo.ToDoWeb.repository.RequestRepository;
 import web.todo.ToDoWeb.service.RequestService;
 
+@Service
 public class RequestServiceImpl extends BaseServiceImpl<Request, String, RequestRepository> implements RequestService {
 
     public RequestServiceImpl(RequestRepository repository) {
