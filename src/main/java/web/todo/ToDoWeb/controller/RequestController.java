311e04e226ee78c793fe06fecc01904b74225304
Matin <matin.parsapour.iam@gmail.com>
2022-03-27 16:33:36 +0430

    Add a method in controller to get all of user requests

--- src/main/java/web/todo/ToDoWeb/controller/RequestController.java
+++ src/main/java/web/todo/ToDoWeb/controller/RequestController.java
@@ -1,14 +1,13 @@
 package web.todo.ToDoWeb.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import web.todo.ToDoWeb.model.Request;
 import web.todo.ToDoWeb.model.dto.RequestDTO;
 import web.todo.ToDoWeb.service.RequestService;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/request")
@@ -24,5 +23,10 @@ public class RequestController {
     @PostMapping("/start-new-request")
     public void startNewRequest(@Valid @RequestBody RequestDTO requestDTO){
         requestService.startNewRequest(requestDTO);
+    }
+
+    @GetMapping("/get-user-requests/{userId}")
+    public List<Request> getUserRequests(@PathVariable("userId") String userId){
+        return requestService.getUserRequests(userId);
     }
 }
