d79fd39869a7e296434d4b1bef84f3fd5722c9b7
Matin <matin.parsapour.iam@gmail.com>
2022-03-27 16:59:54 +0430

    Use another method to delete important user properties for each request in the method that returns list of requests

--- src/main/java/web/todo/ToDoWeb/service/impl/RequestServiceImpl.java
+++ src/main/java/web/todo/ToDoWeb/service/impl/RequestServiceImpl.java
@@ -80,19 +80,7 @@ public class RequestServiceImpl extends BaseServiceImpl<Request, String, Request
     }
 
     private List<Request> makeImportantPropertiesOfRequestsNull(List<Request> requests){
-        requests.forEach(request -> {
-            request.getUser().setPassword(null);
-            request.getUser().setRole(null);
-            request.getUser().setIsBlocked(null);
-            request.getUser().setAuthorities(null);
-            request.getUser().setValidatorEmail(null);
-            request.getUser().setIsDeleted(null);
-            request.getUser().setProfileImageUrl(null);
-            request.getUser().setBirthDay(null);
-            request.getUser().setPhoneNumber(null);
-            request.getUser().setToDoFolders(null);
-            request.getUser().setAge(0);
-        });
+        requests.forEach(this::makeImportantPropertiesOfRequestNull);
         return requests;
     }
 
