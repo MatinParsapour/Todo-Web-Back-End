ec574471f06b2b0c87487f2039b5bde4d66e41ae
Matin <matin.parsapour.iam@gmail.com>
2022-03-27 17:37:20 +0430

    Add a new method in MessageService to get data and save new message

--- src/main/java/web/todo/ToDoWeb/service/MessageService.java
+++ src/main/java/web/todo/ToDoWeb/service/MessageService.java
@@ -1,6 +1,9 @@
 package web.todo.ToDoWeb.service;
 
 import web.todo.ToDoWeb.model.Message;
+import web.todo.ToDoWeb.model.dto.MessageDTO;
 
 public interface MessageService extends BaseService<Message, String> {
+
+    void sendMessage(String requestId, MessageDTO messageDTO);
 }
