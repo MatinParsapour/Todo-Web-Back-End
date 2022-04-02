2500c6d951bacf4c97ef96c4cb1d7695cbc211e6
Matin <matin.parsapour.iam@gmail.com>
2022-04-02 14:30:04 +0430

    Create a class to extract id of user when use mongo group to find users who have request

---
+++ src/main/java/web/todo/ToDoWeb/model/RequestId.java
@@ -0,0 +1,15 @@
+package web.todo.ToDoWeb.model;
+
+import lombok.Data;
+
+@Data
+public class RequestId {
+
+    private Object _id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+}
