48ece6215277c6e8e7e36ad0437e36841fff8d6e
Matin <matin.parsapour.iam@gmail.com>
2022-04-03 16:10:35 +0430

    Add two methods one to add image and one to get image

--- src/main/java/web/todo/ToDoWeb/controller/MessageController.java
+++ src/main/java/web/todo/ToDoWeb/controller/MessageController.java
@@ -1,9 +1,19 @@
 package web.todo.ToDoWeb.controller;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import web.todo.ToDoWeb.model.Message;
 import web.todo.ToDoWeb.model.dto.MessageDTO;
 import web.todo.ToDoWeb.service.MessageService;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.springframework.http.MediaType.IMAGE_JPEG_VALUE;
+import static web.todo.ToDoWeb.constants.FileConstants.FORWARD_SLASH;
+import static web.todo.ToDoWeb.constants.FileConstants.REQUEST_FOLDER;
 
 @RestController
 @RequestMapping("/message")
@@ -18,6 +28,16 @@ public class MessageController {
     @PostMapping("/send-message/{requestId}")
     public void sendMessage(@PathVariable("requestId") String requestId, @RequestBody MessageDTO messageDTO){
         messageService.sendMessage(requestId, messageDTO);
+    }
+
+    @PostMapping("/send-picture")
+    public void sendPicture(@RequestParam("requestId") String requestId, @RequestParam("picture")MultipartFile picture, @RequestParam("userId") String userId) throws IOException {
+        messageService.sendPicture(requestId, picture, userId);
+    }
+
+    @GetMapping(value = "/image/{userId}/{requestId}/{fileName}", produces = IMAGE_JPEG_VALUE)
+    public byte[] getPicture(@PathVariable("userId") String userId, @PathVariable("requestId") String requestId, @PathVariable("fileName") String fileName) throws IOException {
+        return Files.readAllBytes(Paths.get(REQUEST_FOLDER + FORWARD_SLASH + userId + FORWARD_SLASH + requestId + FORWARD_SLASH + fileName));
     }
 
     @PutMapping("/update-message")
