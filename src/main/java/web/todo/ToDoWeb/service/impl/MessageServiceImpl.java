de486f56c443055306bae93c486bf76ab31f7799
Matin <matin.parsapour.iam@gmail.com>
2022-04-03 16:11:37 +0430

    Implement add picture method with necessary method

--- src/main/java/web/todo/ToDoWeb/service/impl/MessageServiceImpl.java
+++ src/main/java/web/todo/ToDoWeb/service/impl/MessageServiceImpl.java
@@ -1,6 +1,8 @@
 package web.todo.ToDoWeb.service.impl;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import web.todo.ToDoWeb.exception.NotFoundException;
 import web.todo.ToDoWeb.model.Message;
 import web.todo.ToDoWeb.model.User;
@@ -9,6 +11,15 @@ import web.todo.ToDoWeb.repository.MessageRepository;
 import web.todo.ToDoWeb.service.MessageService;
 import web.todo.ToDoWeb.service.RequestService;
 import web.todo.ToDoWeb.service.UserService;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import static org.springframework.http.MediaType.*;
+import static web.todo.ToDoWeb.constants.FileConstants.*;
 
 @Service
 public class MessageServiceImpl extends BaseServiceImpl<Message, String, MessageRepository> implements MessageService {
@@ -48,6 +59,30 @@ public class MessageServiceImpl extends BaseServiceImpl<Message, String, Message
             save(message);
         } else {
             throw new NotFoundException("The message might be deleted");
+        }
+    }
+
+    @Override
+    public void sendPicture(String requestId, MultipartFile picture, String userId) throws IOException {
+        validImageTypeAssertion(picture);
+        Path requestFolder = Paths.get(REQUEST_FOLDER + userId + FORWARD_SLASH + requestId).toAbsolutePath().normalize();
+        if (!Files.exists(requestFolder)){
+            Files.createDirectories(requestFolder);
+        }
+        Files.copy(picture.getInputStream(), requestFolder.resolve(picture.getOriginalFilename()));
+        MessageDTO messageDTO = new MessageDTO();
+        messageDTO.setMessage(setPictureImageUrl(userId, requestId, picture));
+        messageDTO.setUserId(userId);
+        sendMessage(requestId, messageDTO);
+    }
+
+    private String setPictureImageUrl(String userId, String requestId, MultipartFile picture){
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path(MESSAGE_IMAGE_PATH + FORWARD_SLASH + userId + FORWARD_SLASH + requestId + FORWARD_SLASH + picture.getOriginalFilename()).toUriString();
+    }
+
+    private void validImageTypeAssertion(MultipartFile profileImage) {
+        if (!Arrays.asList(IMAGE_JPEG_VALUE, IMAGE_PNG_VALUE, IMAGE_GIF_VALUE).contains(profileImage.getContentType())) {
+            throw new IllegalStateException(profileImage.getOriginalFilename() + " is not a suitable file please upload image");
         }
     }
 
