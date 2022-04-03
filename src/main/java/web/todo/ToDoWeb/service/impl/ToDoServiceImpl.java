7cae3eebcfdb60b499b8683d03c605043d7a6a12
Matin <matin.parsapour.iam@gmail.com>
2022-04-03 07:44:52 +0430

    Update save todo in category method and use user id string instead of user object

--- src/main/java/web/todo/ToDoWeb/service/impl/ToDoServiceImpl.java
+++ src/main/java/web/todo/ToDoWeb/service/impl/ToDoServiceImpl.java
@@ -82,7 +82,7 @@ public class ToDoServiceImpl extends BaseServiceImpl<ToDo, String, ToDoRepositor
     }
 
     @Override
-    public void saveToDoInCategory(ToDo toDo, User user) {
+    public void saveToDoInCategory(ToDo toDo, String userId) {
         if (isEmpty(toDo.getTask()) || isBlank(toDo.getTask()) || isNull(toDo.getTask()) || isWhiteSpace(toDo.getTask())) {
             throw new EmptyException("For to do at least you should fill task");
         }
