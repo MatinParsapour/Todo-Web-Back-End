9f6fba0108e46d71db54e034d3793263a96164ea
Matin <matin.parsapour.iam@gmail.com>
2022-04-03 16:10:08 +0430

    Update file constant for messages images

--- src/main/java/web/todo/ToDoWeb/constants/FileConstants.java
+++ src/main/java/web/todo/ToDoWeb/constants/FileConstants.java
@@ -3,10 +3,12 @@ package web.todo.ToDoWeb.constants;
 public class FileConstants {
     public static final String USER_FOLDER = System.getProperty("D:\\pictures") + "/users/user/";
     public static final String TODO_FOLDER = System.getProperty("D:\\pictures") + "/todos/todo/";
+    public static final String REQUEST_FOLDER = System.getProperty("D:\\pictures") + "/requests/request/";
     public static final String DIRECTORY_CREATED = "Create directory for: ";
     public static final String DOT = ".";
     public static final String JPG_EXTENSION = "jpg";
     public static final String USER_IMAGE_PATH = "/user/image/";
     public static final String TODO_IMAGE_PATH = "/to-do/image/";
+    public static final String MESSAGE_IMAGE_PATH = "/message/image";
     public static final String FORWARD_SLASH = "/";
 }
