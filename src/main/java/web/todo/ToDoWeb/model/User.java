8a24f295f75bc829c1ba2f1452f4c82759859b81
Matin <matin.parsapour.iam@gmail.com>
2022-04-04 14:17:09 +0430

    Update relationship between user and todo

--- src/main/java/web/todo/ToDoWeb/model/User.java
+++ src/main/java/web/todo/ToDoWeb/model/User.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import web.todo.ToDoWeb.enumeration.Role;
 import web.todo.ToDoWeb.util.AES;
@@ -50,6 +51,7 @@ public class User {
 
     private Boolean isDeleted = Boolean.FALSE;
 
+    @DBRef
     private Set<ToDo> toDos = new HashSet<>();
 
     private Set<ToDoFolder> toDoFolders = new TreeSet<>();
