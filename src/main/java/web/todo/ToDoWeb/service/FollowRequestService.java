a30d1c90a7b5708441aa9832288f6fb50a825b97
Matin <matin.parsapour.iam@gmail.com>
2022-04-15 14:48:01 +0430

    Add a method in service to get applicant id and responder id

--- src/main/java/web/todo/ToDoWeb/service/FollowRequestService.java
+++ src/main/java/web/todo/ToDoWeb/service/FollowRequestService.java
@@ -3,4 +3,6 @@ package web.todo.ToDoWeb.service;
 import web.todo.ToDoWeb.model.FollowRequest;
 
 public interface FollowRequestService extends BaseService<FollowRequest, String> {
+
+    void followRequest(String applicantId, String responderId);
 }
