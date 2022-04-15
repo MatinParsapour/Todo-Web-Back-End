f27360f534fb1e7501c4610961ac347a24e79616
Matin <matin.parsapour.iam@gmail.com>
2022-04-15 14:48:35 +0430

    Implement the method to create new FollowRequest and set applicant id and responder id and save in data base

--- src/main/java/web/todo/ToDoWeb/service/impl/FollowRequestServiceImpl.java
+++ src/main/java/web/todo/ToDoWeb/service/impl/FollowRequestServiceImpl.java
@@ -1,14 +1,31 @@
 package web.todo.ToDoWeb.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import web.todo.ToDoWeb.enumeration.FollowRequestStatus;
 import web.todo.ToDoWeb.model.FollowRequest;
 import web.todo.ToDoWeb.repository.FollowRequestRepository;
 import web.todo.ToDoWeb.service.FollowRequestService;
+import web.todo.ToDoWeb.service.UserService;
 
 @Service
 public class FollowRequestServiceImpl extends BaseServiceImpl<FollowRequest, String, FollowRequestRepository> implements FollowRequestService {
 
-    public FollowRequestServiceImpl(FollowRequestRepository repository) {
+    private final FollowRequestRepository followRequestRepository;
+    private final UserService userService;
+
+    @Autowired
+    public FollowRequestServiceImpl(FollowRequestRepository repository, UserService userService) {
         super(repository);
+        this.followRequestRepository = repository;
+        this.userService = userService;
+    }
+
+    @Override
+    public void followRequest(String applicantId, String responderId) {
+        FollowRequest followRequest = new FollowRequest();
+        followRequest.setResponder(userService.getUserById(responderId));
+        followRequest.setApplicant(userService.getUserById(applicantId));
+        save(followRequest);
     }
 }
