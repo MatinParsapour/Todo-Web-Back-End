f9645215c28a5270622d9ebccbf929dbee472804
Matin <matin.parsapour.iam@gmail.com>
2022-04-15 15:41:40 +0430

    Add a method in FollowRequestService to get responder id and return list of follow requests

--- src/main/java/web/todo/ToDoWeb/service/FollowRequestService.java
+++ src/main/java/web/todo/ToDoWeb/service/FollowRequestService.java
@@ -3,9 +3,13 @@ package web.todo.ToDoWeb.service;
 import web.todo.ToDoWeb.enumeration.FollowRequestStatus;
 import web.todo.ToDoWeb.model.FollowRequest;
 
+import java.util.List;
+
 public interface FollowRequestService extends BaseService<FollowRequest, String> {
 
     void followRequest(String applicantId, String responderId);
 
     void changeFollowRequestStatus(FollowRequestStatus status, String requestId);
+
+    List<FollowRequest> getAllUserFollowRequests(String responderId);
 }
