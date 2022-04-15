2034abe80854cfa484dd25852277c6306727fb22
Matin <matin.parsapour.iam@gmail.com>
2022-04-15 15:25:24 +0430

    Add a method to get new status and request id and update status

--- src/main/java/web/todo/ToDoWeb/service/FollowRequestService.java
+++ src/main/java/web/todo/ToDoWeb/service/FollowRequestService.java
@@ -1,8 +1,11 @@
 package web.todo.ToDoWeb.service;
 
+import web.todo.ToDoWeb.enumeration.FollowRequestStatus;
 import web.todo.ToDoWeb.model.FollowRequest;
 
 public interface FollowRequestService extends BaseService<FollowRequest, String> {
 
     void followRequest(String applicantId, String responderId);
+
+    void changeFollowRequestStatus(FollowRequestStatus status, String requestId);
 }
