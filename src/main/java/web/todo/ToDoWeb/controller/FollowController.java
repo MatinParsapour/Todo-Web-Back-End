adc83cecc8348012f9d9813c8c218dc0ebd4f0ec
Matin <matin.parsapour.iam@gmail.com>
2022-04-15 14:33:54 +0430

    Create a controller for follow requests

---
+++ src/main/java/web/todo/ToDoWeb/controller/FollowController.java
@@ -0,0 +1,9 @@
+package web.todo.ToDoWeb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/follow-request")
+public class FollowController {
+}
