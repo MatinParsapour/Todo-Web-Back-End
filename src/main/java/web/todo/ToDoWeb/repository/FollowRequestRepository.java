005c7bb54ddaf3556c23b0ae7d5672ac46fbe5ff
Matin <matin.parsapour.iam@gmail.com>
2022-04-15 15:41:14 +0430

    Add a method in FollowRequestRepository to find a list of requests based on responder id and request status

--- src/main/java/web/todo/ToDoWeb/repository/FollowRequestRepository.java
+++ src/main/java/web/todo/ToDoWeb/repository/FollowRequestRepository.java
@@ -1,7 +1,13 @@
 package web.todo.ToDoWeb.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import web.todo.ToDoWeb.enumeration.FollowRequestStatus;
 import web.todo.ToDoWeb.model.FollowRequest;
+import web.todo.ToDoWeb.model.User;
+
+import java.util.List;
 
 public interface FollowRequestRepository extends MongoRepository<FollowRequest, String> {
+
+    List<FollowRequest> findAllByResponderAndStatus(User responderId, FollowRequestStatus status);
 }
