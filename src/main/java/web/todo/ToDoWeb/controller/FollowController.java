3c4af8157b48a4685a222d4df1b9176cf8d333e3
Matin <matin.parsapour.iam@gmail.com>
2022-04-15 15:23:28 +0430

    Add a method to update status of follow request

--- src/main/java/web/todo/ToDoWeb/controller/FollowController.java
+++ src/main/java/web/todo/ToDoWeb/controller/FollowController.java
@@ -1,10 +1,8 @@
 package web.todo.ToDoWeb.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import web.todo.ToDoWeb.enumeration.FollowRequestStatus;
 import web.todo.ToDoWeb.service.FollowRequestService;
 
 @RestController
@@ -21,5 +19,10 @@ public class FollowController {
     @PostMapping("/send-follow-request")
     public void followRequest(@RequestParam("applicantId") String applicantId, @RequestParam("responderId") String responderId){
         followRequest.followRequest(applicantId, responderId);
+    }
+
+    @PutMapping("/change-follow-request-status")
+    public void changeFollowRequestStatus(@RequestParam("status")FollowRequestStatus status, @RequestParam("requestId") String requestId){
+        followRequest.changeFollowRequestStatus(status, requestId);
     }
 }
