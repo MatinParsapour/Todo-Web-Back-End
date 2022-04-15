bcaf5763b841d090ceccf70d06b411c40b0dc217
Matin <matin.parsapour.iam@gmail.com>
2022-04-15 14:47:01 +0430

    Add a method in controller to get applicant id and responder id and send to service

--- src/main/java/web/todo/ToDoWeb/controller/FollowController.java
+++ src/main/java/web/todo/ToDoWeb/controller/FollowController.java
@@ -1,9 +1,25 @@
 package web.todo.ToDoWeb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import web.todo.ToDoWeb.service.FollowRequestService;
 
 @RestController
 @RequestMapping("/follow-request")
 public class FollowController {
+
+    private final FollowRequestService followRequest;
+
+    @Autowired
+    public FollowController(FollowRequestService followRequest) {
+        this.followRequest = followRequest;
+    }
+
+    @PostMapping("/send-follow-request")
+    public void followRequest(@RequestParam("applicantId") String applicantId, @RequestParam("responderId") String responderId){
+        followRequest.followRequest(applicantId, responderId);
+    }
 }
