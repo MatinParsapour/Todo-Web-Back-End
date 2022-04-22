cb758bcceb0f67293b3c876ebeb4f7bde363be8f
Matin <matin.parsapour.iam@gmail.com>
2022-04-22 09:52:10 +0430

    Remove unused constant

--- src/main/java/web/todo/ToDoWeb/constants/FileConstants.java
+++ src/main/java/web/todo/ToDoWeb/constants/FileConstants.java
@@ -4,7 +4,6 @@ public class FileConstants {
     public static final String USER_FOLDER = System.getProperty("D:\\pictures") + "/users/user/";
     public static final String TODO_FOLDER = System.getProperty("D:\\pictures") + "/todos/todo/";
     public static final String REQUEST_FOLDER = System.getProperty("D:\\pictures") + "/requests/request/";
-    public static final String DIRECTORY_CREATED = "Create directory for: ";
     public static final String DOT = ".";
     public static final String JPG_EXTENSION = "jpg";
     public static final String USER_IMAGE_PATH = "/user/image/";
