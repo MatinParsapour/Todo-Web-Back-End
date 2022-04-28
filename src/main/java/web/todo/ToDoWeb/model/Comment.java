d62fdebb6fc3d8b3b0ef137f06ed43145c0cf1ce
Matin <matin.parsapour.iam@gmail.com>
2022-04-28 18:50:13 +0430

    Create a class named comment for user to send comment

---
+++ src/main/java/web/todo/ToDoWeb/model/Comment.java
@@ -0,0 +1,25 @@
+package web.todo.ToDoWeb.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = Comment.COMMENT_DOCUMENT)
+@Data
+public class Comment implements Comparable<Comment> {
+    public static final String COMMENT_DOCUMENT = "comment_document";
+
+    @Id
+    private String id;
+
+    @DBRef
+    private User user;
+
+    private String message;
+
+    @Override
+    public int compareTo(Comment o) {
+        return this.id.compareTo(o.id);
+    }
+}
