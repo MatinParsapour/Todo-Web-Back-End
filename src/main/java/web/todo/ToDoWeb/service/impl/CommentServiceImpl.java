582b9e6b5397c53b62c46a9d0f8bb29e50ca10e3
Matin <matin.parsapour.iam@gmail.com>
2022-04-28 19:14:11 +0430

    Implement adding comment in database and a method to build new Comment document

--- src/main/java/web/todo/ToDoWeb/service/impl/CommentServiceImpl.java
+++ src/main/java/web/todo/ToDoWeb/service/impl/CommentServiceImpl.java
@@ -1,15 +1,39 @@
 package web.todo.ToDoWeb.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import web.todo.ToDoWeb.model.Comment;
+import web.todo.ToDoWeb.model.dto.CommentDTO;
 import web.todo.ToDoWeb.repository.CommentRepository;
 import web.todo.ToDoWeb.service.CommentService;
+import web.todo.ToDoWeb.service.ToDoService;
+import web.todo.ToDoWeb.service.UserService;
 
 @Service
 public class CommentServiceImpl extends BaseServiceImpl<Comment, String, CommentRepository>
         implements CommentService {
 
-    public CommentServiceImpl(CommentRepository repository) {
+    private final UserService userService;
+    private final ToDoService toDoService;
+
+    @Autowired
+    public CommentServiceImpl(CommentRepository repository, UserService userService, ToDoService toDoService) {
         super(repository);
+        this.userService = userService;
+        this.toDoService = toDoService;
+    }
+
+    @Override
+    public void addComment(CommentDTO comment) {
+        Comment newComment = createCommentByCommentDTO(comment);
+        save(newComment);
+        toDoService.addCommentToComments(newComment, comment.getTodoId());
+    }
+
+    private Comment createCommentByCommentDTO(CommentDTO comment) {
+        return Comment.builder()
+                .user(userService.findById(comment.getUserId()).get())
+                .message(comment.getMessage())
+                .build();
     }
 }
