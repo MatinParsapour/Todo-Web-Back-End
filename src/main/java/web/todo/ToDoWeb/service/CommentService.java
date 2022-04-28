50481af88e3a3e6880c5cb89ff3666b48453668c
Matin <matin.parsapour.iam@gmail.com>
2022-04-28 19:13:33 +0430

    Add a method to get a comment dto and save new comment in database

--- src/main/java/web/todo/ToDoWeb/service/CommentService.java
+++ src/main/java/web/todo/ToDoWeb/service/CommentService.java
@@ -1,6 +1,9 @@
 package web.todo.ToDoWeb.service;
 
 import web.todo.ToDoWeb.model.Comment;
+import web.todo.ToDoWeb.model.dto.CommentDTO;
 
 public interface CommentService extends BaseService<Comment, String> {
+
+    void addComment(CommentDTO comment);
 }
