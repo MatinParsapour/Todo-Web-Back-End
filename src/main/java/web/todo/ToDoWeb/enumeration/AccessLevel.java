6a9f23f02e12df3be1fbc34bc5fccc26d2d2a20f
Matin <matin.parsapour.iam@gmail.com>
2022-06-25 14:05:04 +0430

    Create an enum for user and todo access level

---
+++ src/main/java/web/todo/ToDoWeb/enumeration/AccessLevel.java
@@ -0,0 +1,10 @@
+package web.todo.ToDoWeb.enumeration;
+
+public enum AccessLevel {
+    /**
+     * PRIVATE: Only user can see users todos
+     * PROTECTED: Only user and followers can see users todos
+     * PUBLIC: Everyone can see users todos
+     */
+    PRIVATE, PROTECTED, PUBLIC
+}
