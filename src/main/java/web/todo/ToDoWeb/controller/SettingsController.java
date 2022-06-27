818b139e381a03120ae60d11533967147350b1bb
Matin <matin.parsapour.iam@gmail.com>
2022-06-27 14:13:09 +0430

    Create a controller for settings

---
+++ src/main/java/web/todo/ToDoWeb/controller/SettingsController.java
@@ -0,0 +1,13 @@
+package web.todo.ToDoWeb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import web.todo.ToDoWeb.model.dto.UserDTO;
+
+@RestController
+@RequestMapping("/settings")
+public class SettingsController {
+
+}
