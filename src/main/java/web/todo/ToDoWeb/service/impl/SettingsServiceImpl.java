3da86e34c563b4ef7343277b8b3074e7b2aa1357
Matin <matin.parsapour.iam@gmail.com>
2022-06-28 16:36:32 +0430

    Implement methods to get user personal and security and account information

--- src/main/java/web/todo/ToDoWeb/service/impl/SettingsServiceImpl.java
+++ src/main/java/web/todo/ToDoWeb/service/impl/SettingsServiceImpl.java
@@ -1,12 +1,33 @@
 package web.todo.ToDoWeb.service.impl;
 
+import org.springframework.stereotype.Service;
+import web.todo.ToDoWeb.exception.NotFoundException;
 import web.todo.ToDoWeb.model.User;
+import web.todo.ToDoWeb.model.dto.UserDTO;
 import web.todo.ToDoWeb.repository.UserRepository;
 import web.todo.ToDoWeb.service.SettingsService;
 
+@Service
 public class SettingsServiceImpl extends BaseServiceImpl<User, String, UserRepository> implements SettingsService {
+    private final UserRepository userRepository;
 
     public SettingsServiceImpl(UserRepository repository) {
         super(repository);
+        this.userRepository = repository;
+    }
+
+    @Override
+    public UserDTO getUserPersonalInfo(String userId) {
+        return userRepository.getUserPersonalInfo(userId).orElseThrow(() -> new NotFoundException("No user found"));
+    }
+
+    @Override
+    public UserDTO getUserSecurityInfo(String userId) {
+        return userRepository.getUserSecurityInfo(userId).orElseThrow(() -> new NotFoundException("No user found"));
+    }
+
+    @Override
+    public UserDTO getUserAccountInfo(String userId) {
+        return userRepository.getUserAccountInfo(userId).orElseThrow(() -> new NotFoundException("No user found"));
     }
 }
