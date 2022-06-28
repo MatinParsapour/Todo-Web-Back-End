4bac5cd08a7e7fcf776c6b6e05f0fc078ac59908
Matin <matin.parsapour.iam@gmail.com>
2022-06-28 16:36:05 +0430

    Add methods in settings service to get user personal and security and account information

--- src/main/java/web/todo/ToDoWeb/service/SettingsService.java
+++ src/main/java/web/todo/ToDoWeb/service/SettingsService.java
@@ -1,4 +1,12 @@
 package web.todo.ToDoWeb.service;
 
+import web.todo.ToDoWeb.model.dto.UserDTO;
+
 public interface SettingsService {
+
+    UserDTO getUserPersonalInfo(String userId);
+
+    UserDTO getUserSecurityInfo(String userId);
+
+    UserDTO getUserAccountInfo(String userId);
 }
