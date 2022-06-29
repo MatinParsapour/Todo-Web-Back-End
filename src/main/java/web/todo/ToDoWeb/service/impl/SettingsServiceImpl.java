ce3d35d4d1dc7fd801e07077f3c0c709d67a48df
Matin <matin.parsapour.iam@gmail.com>
2022-06-29 17:26:36 +0430

    Implement the method to update user personal info

--- src/main/java/web/todo/ToDoWeb/service/impl/SettingsServiceImpl.java
+++ src/main/java/web/todo/ToDoWeb/service/impl/SettingsServiceImpl.java
@@ -42,4 +42,14 @@ public class SettingsServiceImpl extends BaseServiceImpl<User, String, UserRepos
         User user = findById(userDTO.getId()).orElseThrow(() -> new NotFoundException("No user found"));
         user.setUserName(userDTO.getUserName());
     }
+
+    @Override
+    public void updatePersonalInfo(UserDTO userDTO) {
+        User user = findById(userDTO.getId()).orElseThrow(() -> new NotFoundException("No user found"));
+        user.setFirstName(userDTO.getFirstName());
+        user.setLastName(userDTO.getLastName());
+        user.setBirthDay(userDTO.getBirthDay());
+        user.setAge(userDTO.getAge());
+        save(user);
+    }
 }
