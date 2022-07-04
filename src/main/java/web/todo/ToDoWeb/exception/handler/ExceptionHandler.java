a6bdc1427798ded231a8e0e3a77b05ca9604f350
Matin <matin.parsapour.iam@gmail.com>
2022-07-04 18:03:17 +0430

    Use getErrorMessage in exceptions to create an object and return that object instead of just a simple text

--- src/main/java/web/todo/ToDoWeb/exception/handler/ExceptionHandler.java
+++ src/main/java/web/todo/ToDoWeb/exception/handler/ExceptionHandler.java
@@ -5,60 +5,62 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import web.todo.ToDoWeb.exception.*;
+import web.todo.ToDoWeb.exception.message.ErrorMessage;
 
 import java.time.format.DateTimeParseException;
+import java.util.Date;
 
 @RestControllerAdvice
 public class ExceptionHandler {
 
     @org.springframework.web.bind.annotation.ExceptionHandler(DoplicateException.class)
-    public ResponseEntity<String> doplicateExceptionHandler(DoplicateException exception){
-        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(exception.getMessage());
+    public ResponseEntity<ErrorMessage> doplicateExceptionHandler(DoplicateException exception){
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(getErrorMessage(exception,HttpStatus.NOT_ACCEPTABLE));
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(EmptyException.class)
-    public ResponseEntity<String> emptyExceptionHandler(EmptyException exception){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    public ResponseEntity<ErrorMessage> emptyExceptionHandler(EmptyException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(getErrorMessage(exception,HttpStatus.BAD_REQUEST));
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(InValidException.class)
-    public ResponseEntity<String> invalidExceptionHandler(InValidException exception){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    public ResponseEntity<ErrorMessage> invalidExceptionHandler(InValidException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(getErrorMessage(exception,HttpStatus.BAD_REQUEST));
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> notFoundExceptionHandler(NotFoundException exception){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    public ResponseEntity<ErrorMessage> notFoundExceptionHandler(NotFoundException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(getErrorMessage(exception,HttpStatus.NOT_FOUND));
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(WeakException.class)
-    public ResponseEntity<String> weakExceptionHandler(WeakException exception){
-        return ResponseEntity.status(HttpStatus.UPGRADE_REQUIRED).body(exception.getMessage());
+    public ResponseEntity<ErrorMessage> weakExceptionHandler(WeakException exception){
+        return ResponseEntity.status(HttpStatus.UPGRADE_REQUIRED).body(getErrorMessage(exception,HttpStatus.UPGRADE_REQUIRED));
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(DateTimeParseException.class)
-    public ResponseEntity<String> dateTimeParseExceptionHandler(DateTimeParseException exception){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    public ResponseEntity<ErrorMessage> dateTimeParseExceptionHandler(DateTimeParseException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(getErrorMessage(exception,HttpStatus.BAD_REQUEST));
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(IllegalStateException.class)
-    public ResponseEntity<String> illegalStateExceptionHandler(IllegalStateException exception){
-        return ResponseEntity.status(HttpStatus.FAILED_DEPENDENCY).body(exception.getMessage());
+    public ResponseEntity<ErrorMessage> illegalStateExceptionHandler(IllegalStateException exception){
+        return ResponseEntity.status(HttpStatus.FAILED_DEPENDENCY).body(getErrorMessage(exception,HttpStatus.FAILED_DEPENDENCY));
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException exception){
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    public ResponseEntity<ErrorMessage> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(getErrorMessage(exception,HttpStatus.BAD_REQUEST));
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(VerificationCodeTimeOutException.class)
-    public ResponseEntity<String> verificationCodeTimeOutExceptionHandler(VerificationCodeTimeOutException exception){
-        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(exception.getMessage());
+    public ResponseEntity<ErrorMessage> verificationCodeTimeOutExceptionHandler(VerificationCodeTimeOutException exception){
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(getErrorMessage(exception,HttpStatus.NOT_ACCEPTABLE));
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(BlockedException.class)
-    public ResponseEntity<String> blockedExceptionHandler(BlockedException exception){
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(exception.getMessage());
+    public ResponseEntity<ErrorMessage> blockedExceptionHandler(BlockedException exception){
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(getErrorMessage(exception,HttpStatus.FORBIDDEN));
     }
 
     private ErrorMessage getErrorMessage(Exception exception, HttpStatus status) {
