8214ad1c046fe9cfaa7c59f031b8427617785352
Matin <matin.parsapour.iam@gmail.com>
2022-07-04 16:23:15 +0430

    Update returning user personal info to return bio with other data too

--- src/main/java/web/todo/ToDoWeb/repository/UserRepository.java
+++ src/main/java/web/todo/ToDoWeb/repository/UserRepository.java
@@ -51,7 +51,7 @@ public interface UserRepository extends MongoRepository<User, String> {
     @Query(fields = "{_id: 1, firstName: 1, lastName: 1, profileImageUrl: 1}")
     User findByToDos(ToDo todo);
 
-    @Query(value = "{'id': ?0}" ,fields = "{_id: 0, firstName: 1, lastName: 1, profileImageUrl: 1, birthDay: 1, age: 1}")
+    @Query(value = "{'id': ?0}" ,fields = "{_id: 0,bio: 1 ,firstName: 1, lastName: 1, profileImageUrl: 1, birthDay: 1, age: 1}")
     Optional<UserDTO> getUserPersonalInfo(String userId);
 
     @Query(value = "{'id': ?0}" ,fields = "{_id: 0, email: 1, phoneNumber: 1, password: 1, userName: 1, registerDate: 1, lastLoginDate: 1}")
