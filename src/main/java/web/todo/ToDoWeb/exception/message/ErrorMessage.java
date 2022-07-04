9695bb9b7de01bff83ee7aee09ee7aadaea9473f
Matin <matin.parsapour.iam@gmail.com>
2022-07-04 18:02:19 +0430

    Update ErrorMessage class and add message type time and status of exception

--- src/main/java/web/todo/ToDoWeb/exception/message/ErrorMessage.java
+++ src/main/java/web/todo/ToDoWeb/exception/message/ErrorMessage.java
@@ -1,5 +1,16 @@
 package web.todo.ToDoWeb.exception.message;
 
-public class ErrorMessage {
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
+import java.util.Date;
+
+@Getter
+@Builder
+public class ErrorMessage {
+    private final String message;
+    private final String type;
+    private final Date time;
+    private final HttpStatus status;
 }
