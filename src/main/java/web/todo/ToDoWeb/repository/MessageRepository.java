437eaa89cb339035a6e37fbcee5e049b42f654ee
Matin <matin.parsapour.iam@gmail.com>
2022-07-13 16:42:27 +0430

    Remove Message repository

--- src/main/java/web/todo/ToDoWeb/repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package web.todo.ToDoWeb.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import web.todo.ToDoWeb.model.Message;
-
-public interface MessageRepository extends MongoRepository<Message, String> {
-
-    Boolean existsByIdAndIsDeletedFalse(String messageId);
-}
