0e5a5cdd6f61b425e1a29649e3eadad7d68da977
Matin <matin.parsapour.iam@gmail.com>
2022-07-14 19:38:10 +0430

    Add necessary annotations

--- src/main/java/web/todo/ToDoWeb/model/Tag.java
+++ src/main/java/web/todo/ToDoWeb/model/Tag.java
@@ -1,4 +1,13 @@
 package web.todo.ToDoWeb.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Builder
 public class Tag {
 }
