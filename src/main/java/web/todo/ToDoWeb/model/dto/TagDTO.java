a1a5a2048283d48bae1183f027feb5ee03366b9b
Matin <matin.parsapour.iam@gmail.com>
2022-07-14 19:59:19 +0430

    Add necessary properties

--- src/main/java/web/todo/ToDoWeb/model/dto/TagDTO.java
+++ src/main/java/web/todo/ToDoWeb/model/dto/TagDTO.java
@@ -2,8 +2,17 @@ package web.todo.ToDoWeb.model.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import web.todo.ToDoWeb.model.ToDo;
+
+import java.util.Set;
 
 @Setter
 @Getter
 public class TagDTO {
+
+    private String id;
+
+    private String name;
+
+    private Set<ToDo> toDos;
 }
