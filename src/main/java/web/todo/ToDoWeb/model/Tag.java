7b3c2c7e8a467ace40af09b7a5858f6e067627fb
Matin <matin.parsapour.iam@gmail.com>
2022-07-14 19:41:30 +0430

    Update tag class by needed properties

--- src/main/java/web/todo/ToDoWeb/model/Tag.java
+++ src/main/java/web/todo/ToDoWeb/model/Tag.java
@@ -4,10 +4,31 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+import java.util.Set;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @Builder
+@Document(collection = Tag.COLLECTION_NAME)
 public class Tag {
+    public static final String COLLECTION_NAME = "tag";
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private Date createdAt;
+
+    @DBRef
+    private User createdBy;
+
+    @DBRef
+    private Set<ToDo> toDos;
 }
