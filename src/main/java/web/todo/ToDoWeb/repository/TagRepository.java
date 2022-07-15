130ec7bb4c1b9d07405401c928dc265d90e892a9
Matin <matin.parsapour.iam@gmail.com>
2022-07-15 07:01:08 +0430

    Add a method to check if tag exists or not

--- src/main/java/web/todo/ToDoWeb/repository/TagRepository.java
+++ src/main/java/web/todo/ToDoWeb/repository/TagRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import web.todo.ToDoWeb.model.Tag;
 
 public interface TagRepository extends MongoRepository<Tag, String> {
+
+    boolean existsByName(String name);
 }
