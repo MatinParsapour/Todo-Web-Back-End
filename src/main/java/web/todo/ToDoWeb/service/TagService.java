d2132d9f0600fa2441c4eb93619f61ddcbe90552
Matin <matin.parsapour.iam@gmail.com>
2022-07-15 07:08:03 +0430

    Add a method to create tag

--- src/main/java/web/todo/ToDoWeb/service/TagService.java
+++ src/main/java/web/todo/ToDoWeb/service/TagService.java
@@ -1,9 +1,12 @@
 package web.todo.ToDoWeb.service;
 
 import org.springframework.stereotype.Service;
+import web.todo.ToDoWeb.model.User;
 
 @Service
 public interface TagService {
 
     boolean getByName(String name);
+
+    void createTag(String tag, User user);
 }
