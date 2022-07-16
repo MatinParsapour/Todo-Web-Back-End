08b16dbbaeae007bc13eeca243c63fc0021da653
Matin <matin.parsapour.iam@gmail.com>
2022-07-16 17:08:30 +0430

    Add a method to find todo by comment

--- src/main/java/web/todo/ToDoWeb/repository/ToDoRepository.java
+++ src/main/java/web/todo/ToDoWeb/repository/ToDoRepository.java
@@ -1,7 +1,12 @@
 package web.todo.ToDoWeb.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import web.todo.ToDoWeb.model.Comment;
 import web.todo.ToDoWeb.model.ToDo;
 
+import java.util.Optional;
+
 public interface ToDoRepository extends MongoRepository<ToDo, String> {
+
+    Optional<ToDo> findByComments(Comment comment);
 }
