bfecdf938dc1dfcd7c90a7c1768efb50b8394ca4
Matin <matin.parsapour.iam@gmail.com>
2022-07-16 17:01:32 +0430

    Add a method to find tags in a comment and create tag

--- src/main/java/web/todo/ToDoWeb/service/impl/CommentServiceImpl.java
+++ src/main/java/web/todo/ToDoWeb/service/impl/CommentServiceImpl.java
@@ -58,4 +58,12 @@ public class CommentServiceImpl extends BaseServiceImpl<Comment, String, Comment
                 .message(comment.getMessage())
                 .build();
     }
+
+    private void findTags(ToDo todo, String section, User user) {
+        Matcher match = Pattern.compile("#([^\\d&%$_-]\\S{2,49})\\b").matcher(section);
+        while (match.find()){
+            String grouped = match.group();
+            tagService.addToDoToTag(todo, grouped, user);
+        }
+    }
 }
