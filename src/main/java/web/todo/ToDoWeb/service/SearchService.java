6907e379c7f33394653df5d8c3efeef6ef151b0e
Matin <matin.parsapour.iam@gmail.com>
2022-07-18 18:52:05 +0430

    Add a method to search by keyword

--- src/main/java/web/todo/ToDoWeb/service/SearchService.java
+++ src/main/java/web/todo/ToDoWeb/service/SearchService.java
@@ -1,4 +1,8 @@
 package web.todo.ToDoWeb.service;
 
+import java.util.List;
+
 public interface SearchService {
+
+    List<Object> search(String keyword);
 }
--- src/main/java/web/todo/ToDoWeb/service/impl/SearchServiceImpl.java
+++ src/main/java/web/todo/ToDoWeb/service/impl/SearchServiceImpl.java
@@ -1,6 +1,28 @@
 package web.todo.ToDoWeb.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import web.todo.ToDoWeb.service.SearchService;
+import web.todo.ToDoWeb.service.TagService;
+import web.todo.ToDoWeb.service.UserService;
 
+import java.util.List;
+
+@Service
 public class SearchServiceImpl implements SearchService {
+
+    private final TagService tagService;
+
+    private final UserService userService;
+
+    @Autowired
+    public SearchServiceImpl(TagService tagService, UserService userService) {
+        this.tagService = tagService;
+        this.userService = userService;
+    }
+
+    @Override
+    public List<Object> search(String keyword) {
+        return null;
+    }
 }
