a4096383c7463f6141d4edefd702ebe72e056ae6
Matin <matin.parsapour.iam@gmail.com>
2022-07-18 18:48:50 +0430

    Make search controller be rest controller and have request mapping

--- src/main/java/web/todo/ToDoWeb/controller/SearchController.java
+++ src/main/java/web/todo/ToDoWeb/controller/SearchController.java
@@ -1,4 +1,9 @@
 package web.todo.ToDoWeb.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/search")
 public class SearchController {
 }
