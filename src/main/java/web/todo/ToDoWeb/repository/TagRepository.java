b9b938b0060cbc84b43d2512f0cf7e9ac8e1bd1c
Matin <matin.parsapour.iam@gmail.com>
2022-07-18 19:50:23 +0430

    Add a method to find tag by name case insensitive

--- src/main/java/web/todo/ToDoWeb/repository/TagRepository.java
+++ src/main/java/web/todo/ToDoWeb/repository/TagRepository.java
@@ -1,11 +1,17 @@
 package web.todo.ToDoWeb.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import web.todo.ToDoWeb.model.Tag;
+
+import java.util.List;
 
 public interface TagRepository extends MongoRepository<Tag, String> {
 
     boolean existsByName(String name);
 
     Tag findByName(String name);
+
+    @Query("{ 'name' : { '$regex' : ?0 , $options: 'i'}}")
+    List<Tag> findByNameContains(String name);
 }
