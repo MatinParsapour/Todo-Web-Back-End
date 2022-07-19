df2d196fbb5db9532f14940a50fc90c4333289ed
Matin <matin.parsapour.iam@gmail.com>
2022-07-19 17:38:19 +0430

    Update tag controller and add a method to get name of tag and return tagdto

--- src/main/java/web/todo/ToDoWeb/controller/TagController.java
+++ src/main/java/web/todo/ToDoWeb/controller/TagController.java
@@ -1,9 +1,26 @@
 package web.todo.ToDoWeb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import web.todo.ToDoWeb.model.dto.TagDTO;
+import web.todo.ToDoWeb.service.TagService;
 
 @RestController
-@RequestMapping("tag")
+@RequestMapping("/tag")
 public class TagController {
+
+    private final TagService tagService;
+
+    @Autowired
+    public TagController(TagService tagService) {
+        this.tagService = tagService;
+    }
+
+    @GetMapping("/get-tag/{tagName}")
+    public TagDTO getTag(@PathVariable("tagName") String tagName) {
+        return tagService.getTagDTO(tagName);
+    }
 }
