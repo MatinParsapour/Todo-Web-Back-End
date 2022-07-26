06a9da88c4ed32d26cab7cfb89a3f230c2e549f0
Matin <matin.parsapour.iam@gmail.com>
2022-07-26 18:38:32 +0430

    Update search method to delete user crucial info

--- src/main/java/web/todo/ToDoWeb/service/impl/SearchServiceImpl.java
+++ src/main/java/web/todo/ToDoWeb/service/impl/SearchServiceImpl.java
@@ -2,12 +2,15 @@ package web.todo.ToDoWeb.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import web.todo.ToDoWeb.model.Tag;
+import web.todo.ToDoWeb.model.User;
 import web.todo.ToDoWeb.service.SearchService;
 import web.todo.ToDoWeb.service.TagService;
 import web.todo.ToDoWeb.service.UserService;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class SearchServiceImpl implements SearchService {
@@ -25,8 +28,32 @@ public class SearchServiceImpl implements SearchService {
     @Override
     public List<Object> search(String keyword) {
         List<Object> objects = new LinkedList<>();
-        objects.addAll(tagService.search(keyword));
-        objects.addAll(userService.search(keyword));
+        List<Tag> tags = tagService.search(keyword);
+        tags = tags.stream().peek(tag -> {
+            tag.setToDos(null);
+            tag.setCreatedBy(null);
+        }).collect(Collectors.toList());
+        List<User> users = userService.search(keyword);
+        users = users.stream().peek(this::removeUserCrucialInfo).collect(Collectors.toList());
+        objects.addAll(tags);
+        objects.addAll(users);
         return objects;
+    }
+
+    private void removeUserCrucialInfo(User user) {
+        user.setLastLoginDate(null);
+        user.setAuthorities(null);
+        user.setIsDeleted(null);
+        user.setToDoFolders(null);
+        user.setAge(0);
+        user.setBirthDay(null);
+        user.setPhoneNumber(null);
+        user.setPassword(null);
+        user.setRole(null);
+        user.setToDos(null);
+        user.setRegisterDate(null);
+        user.setFollowings(null);
+        user.setFollowers(null);
+        user.setIsBlocked(null);
     }
 }
