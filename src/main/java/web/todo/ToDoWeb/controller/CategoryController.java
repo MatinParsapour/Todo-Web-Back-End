967547f4831c000b668ba63ab39f7c60d71330e6
Matin <matin.parsapour.iam@gmail.com>
2022-08-01 10:10:59 +0430

    Update general style for CategoryController

--- src/main/java/web/todo/ToDoWeb/controller/CategoryController.java
+++ src/main/java/web/todo/ToDoWeb/controller/CategoryController.java
@@ -1,7 +1,10 @@
 package web.todo.ToDoWeb.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import web.todo.ToDoWeb.model.ToDo;
 import web.todo.ToDoWeb.service.CategoryFactory;
 
@@ -19,7 +22,7 @@ public class CategoryController {
     }
 
     @GetMapping("/get-category-to-dos/{categoryName}/{userId}")
-    public Set<ToDo> getCategoryToDos(@PathVariable("categoryName") String categoryName, @PathVariable("userId") String userId){
+    public Set<ToDo> getCategoryToDos(@PathVariable("categoryName") String categoryName, @PathVariable("userId") String userId) {
         return categoryFactory.getToDosByCategory(categoryName, userId);
     }
 }
