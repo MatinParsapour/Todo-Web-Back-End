428e2ba6636b43b35ff43194551e6d04226b6d44
Matin <matin.parsapour.iam@gmail.com>
2022-08-01 10:09:45 +0430

    Update general style for Authority enum

--- src/main/java/web/todo/ToDoWeb/constants/Authority.java
+++ src/main/java/web/todo/ToDoWeb/constants/Authority.java
@@ -1,8 +1,8 @@
 package web.todo.ToDoWeb.constants;
 
 public class Authority {
-    public static final String[] USER_AUTHORITY = {"todo:create", "todo:update","todo:delete","todo:read"};
-    public static final String[] MANAGER_AUTHORITY = {"todo:create", "todo:update","todo:delete","todo:read", "user:read", "user:update"};
-    public static final String[] ADMIN_AUTHORITY = {"todo:create", "todo:update","todo:delete","todo:read", "user:read", "user:update", "user:create"};
-    public static final String[] SUPER_ADMIN_AUTHORITY = {"todo:create", "todo:update","todo:delete","todo:read", "user:read", "user:update", "user:create", "user:delete"};
+    public static final String[] USER_AUTHORITY = {"todo:create", "todo:update", "todo:delete", "todo:read"};
+    public static final String[] MANAGER_AUTHORITY = {"todo:create", "todo:update", "todo:delete", "todo:read", "user:read", "user:update"};
+    public static final String[] ADMIN_AUTHORITY = {"todo:create", "todo:update", "todo:delete", "todo:read", "user:read", "user:update", "user:create"};
+    public static final String[] SUPER_ADMIN_AUTHORITY = {"todo:create", "todo:update", "todo:delete", "todo:read", "user:read", "user:update", "user:create", "user:delete"};
 }
