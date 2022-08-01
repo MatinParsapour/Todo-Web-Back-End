eb245c3b67f1d6147780d07333f33d5563af5ad3
Matin <matin.parsapour.iam@gmail.com>
2022-08-01 10:11:07 +0430

    Update general style for CategoryFactory

--- src/main/java/web/todo/ToDoWeb/service/CategoryFactory.java
+++ src/main/java/web/todo/ToDoWeb/service/CategoryFactory.java
@@ -1,10 +1,7 @@
 package web.todo.ToDoWeb.service;
 
 import web.todo.ToDoWeb.model.ToDo;
-import web.todo.ToDoWeb.model.ToDoFolder;
-import web.todo.ToDoWeb.model.User;
 
-import java.util.List;
 import java.util.Set;
 
 public interface CategoryFactory {
