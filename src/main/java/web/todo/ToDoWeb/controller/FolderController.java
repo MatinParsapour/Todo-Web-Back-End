b0eac107afac0b31866f8df0ef7236eb2301af44
Matin <matin.parsapour.iam@gmail.com>
2022-08-02 19:38:05 +0430

    Update general style for FolderController

--- src/main/java/web/todo/ToDoWeb/controller/FolderController.java
+++ src/main/java/web/todo/ToDoWeb/controller/FolderController.java
@@ -21,32 +21,32 @@ public class FolderController {
     }
 
     @PutMapping("/insert-folder")
-    public void insertFolder(@RequestBody FolderDTO folderDTO){
+    public void insertFolder(@RequestBody FolderDTO folderDTO) {
         folderService.addFolder(folderDTO.getUserId(), folderDTO.getFolderName());
     }
 
     @GetMapping("/exists-by-folder-name/{folderName}/{userId}")
-    public Boolean existsByFolderName(@PathVariable("folderName") String folderName, @PathVariable("userId") String userId){
+    public Boolean existsByFolderName(@PathVariable("folderName") String folderName, @PathVariable("userId") String userId) {
         return folderService.existsByToDoFolderName(folderName, userId);
     }
 
     @GetMapping("/get-todo-folders/{userId}")
-    public Set<ToDoFolder> getToDoFolders(@PathVariable("userId") String userId){
+    public Set<ToDoFolder> getToDoFolders(@PathVariable("userId") String userId) {
         return folderService.getUserFolders(userId);
     }
 
     @GetMapping("/get-todo-folder/{toDoFolderName}/{userId}")
-    public Set<ToDoFolder> getToDoFolder(@PathVariable("toDoFolderName") String toDoFolderName, @PathVariable("userId") String userId){
-        return folderService.getToDoFolder(userId,toDoFolderName);
+    public Set<ToDoFolder> getToDoFolder(@PathVariable("toDoFolderName") String toDoFolderName, @PathVariable("userId") String userId) {
+        return folderService.getToDoFolder(userId, toDoFolderName);
     }
 
     @PutMapping("/change-folder-name")
-    public void changeFolderName(@RequestBody UpdateFolderDTO updateFolderDTO){
-        folderService.changeFolderName(updateFolderDTO.getOldName(),updateFolderDTO.getNewName(),updateFolderDTO.getUserId());
+    public void changeFolderName(@RequestBody UpdateFolderDTO updateFolderDTO) {
+        folderService.changeFolderName(updateFolderDTO.getOldName(), updateFolderDTO.getNewName(), updateFolderDTO.getUserId());
     }
 
     @DeleteMapping("/delete-folder/{userId}/{folderName}")
-    public void deleteFolder(@PathVariable("userId") String userId, @PathVariable("folderName") String folderName){
-        folderService.deleteFolder(folderName,userId);
+    public void deleteFolder(@PathVariable("userId") String userId, @PathVariable("folderName") String folderName) {
+        folderService.deleteFolder(folderName, userId);
     }
 }
