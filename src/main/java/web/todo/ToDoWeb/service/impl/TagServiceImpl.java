e49f64284e4d4e3d2718f3c8cd77259e1971c43a
Matin <matin.parsapour.iam@gmail.com>
2022-08-02 19:40:30 +0430

    Update general style for TagServiceImpl

--- src/main/java/web/todo/ToDoWeb/service/impl/TagServiceImpl.java
+++ src/main/java/web/todo/ToDoWeb/service/impl/TagServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 @Service
-public class TagServiceImpl extends BaseServiceImpl<Tag, String, TagRepository> implements TagService  {
+public class TagServiceImpl extends BaseServiceImpl<Tag, String, TagRepository> implements TagService {
 
     public TagServiceImpl(TagRepository repository) {
         super(repository);
