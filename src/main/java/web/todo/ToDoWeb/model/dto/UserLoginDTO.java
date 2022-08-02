5552d3461473f5eb84d949c0ba612d0ecb3545aa
Matin <matin.parsapour.iam@gmail.com>
2022-08-02 19:42:08 +0430

    Update general style for UserLoginDTO

--- src/main/java/web/todo/ToDoWeb/model/dto/UserLoginDTO.java
+++ src/main/java/web/todo/ToDoWeb/model/dto/UserLoginDTO.java
@@ -9,6 +9,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+
 @Getter
 @Setter
 @AllArgsConstructor
