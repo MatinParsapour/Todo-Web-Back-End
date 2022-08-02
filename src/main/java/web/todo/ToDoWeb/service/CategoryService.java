38a112dc14a583be5b221cf73c0e7c56fe99a0c7
Matin <matin.parsapour.iam@gmail.com>
2022-08-02 19:36:51 +0430

    Update general style for CategoryService

--- src/main/java/web/todo/ToDoWeb/service/CategoryService.java
+++ src/main/java/web/todo/ToDoWeb/service/CategoryService.java
@@ -1,13 +1,10 @@
 package web.todo.ToDoWeb.service;
 
 import web.todo.ToDoWeb.model.ToDo;
-import web.todo.ToDoWeb.model.ToDoFolder;
-import web.todo.ToDoWeb.model.User;
 
-import java.util.List;
 import java.util.Set;
 
-public interface CategoryService{
+public interface CategoryService {
 
     Set<ToDo> get(String username);
 }
